75bf0cb119657486ca86071fdfb364a9443cb7b8
Flavio Azeredo da Silva <flaasilva@yahoo.com.br>
2023-06-25 20:06:29 -0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    INCLINACAO_H=4
+    INCLINACAO_V=10
+    ABERTURA_DO_OLHO = 0.15
+    QUANTIDADE_DE_FRAMES = 3
+    PAUSA = 0
+    DISTANCIA=20
+    BOCA_ABERTA=3
+    def __init__(self):
+        print('Classe Settings')
